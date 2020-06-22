603067895521c6867cf90d8610ddb16f92b94fbc
Andrew Coutts <andrewjohncoutts@gmail.com>
2020-06-22 18:30:22 -0400

    Added dart doc comments and bumped version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.0
+* Added dart doc comments.
+
 ## 0.0.2
 * Updated readme to include link to ShowTime package.
 
--- lib/flutter_showtime.dart
+++ lib/flutter_showtime.dart
@@ -6,10 +6,13 @@ import 'package:flutter/services.dart';
 class FlutterShowtime {
   static const MethodChannel _channel = const MethodChannel('flutter_showtime');
 
+  /// Enable ShowTime to make touch events visible.
   static Future<void> enable() async =>
       defaultTargetPlatform == TargetPlatform.iOS
           ? _channel.invokeMethod('enable')
           : null;
+
+  /// Disable ShowTime to hide touch events.
   static Future<void> disable() async =>
       defaultTargetPlatform == TargetPlatform.iOS
           ? _channel.invokeMethod('disable')
